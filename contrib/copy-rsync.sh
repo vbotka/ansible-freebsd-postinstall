6a1a6735a63bab6431b7e534debdd78299476b17
Vladimir Botka <vbotka@gmail.com>
2020-03-14 21:01:16 +0100

    Updated contrib/copy-rsync.sh

--- contrib/copy-rsync.sh
+++ contrib/copy-rsync.sh
@@ -1,71 +1,80 @@
 #!/bin/bash
-#
+
+# All rights reserved (c) 2020, Vladimir Botka <vbotka@gmail.com>
+# Simplified BSD License, https://opensource.org/licenses/BSD-2-Clause
+
+# This file is part of Ansible role vbotka.freebsd_postinstall
+# https://galaxy.ansible.com/vbotka/freebsd_postinstall
+# https://github.com/vbotka/ansible-freebsd-postinstall/blob/master/contrib/copy-rsync.sh
+
 # Synchronize data from HOSTS/DIRS to COPYDIR.
-# Data can be used with Ansible role freebsd-postinstall.
+# Data can be used in Ansible role vbotka.freebsd_postinstall
 # Example:
 # ansible-playbook -e 'fp_synchronize_optional=yes' \
 # ~/.ansible/playbooks/srv.example.com-freebsd-postinstall.yml \
 # -t fp_synchronize_optional
 
+VERSION="1.0"
+
 RSYNC="/usr/bin/rsync"
-RSYNCPARAM="avzl"
+RSYNCPARAM="avzli"
 SSH="/usr/bin/ssh"
-COPYDIR="$HOME/.ansible/playbooks/copy"
+COPYDIR="$HOME/.ansible/copy"
 REMOTEUSER="admin"
-#DRYRUN="-n"
-DRYRUN=""
+DRYRUN="-n"
+# DRYRUN=""
 VERBOSE="1"
 
 function sync {
     for i in $HOSTS; do
 	if ($SSH -q -o "BatchMode=yes" -o "ConnectTimeout=3" $REMOTEUSER@$i :); then
-	    (($VERBOSE)) && printf "* $i *\n"
+	    (($VERBOSE)) && printf "[OK] $i connected.\n"
 	    for j in $DIRS; do
-		(($VERBOSE)) && printf "  $j "
 		if LOG=$($SSH $REMOTEUSER@$i "[ -d $j ]" 2>&1); then
-	            (($VERBOSE)) && printf "exists. "
+	            (($VERBOSE)) && printf "[OK] $j exists.\n "
 		    if [ ! -e "$COPYDIR/$i/$j" ]; then
 			mkdir -p $COPYDIR/$i$j
 		    fi
 		    cd $COPYDIR/$i$j
-		    if LOG=$($RSYNC $DRYRUN -$RSYNCPARAM -e "ssh" --progress --rsync-path="sudo rsync" \
-				    $REMOTEUSER@$i:$j . 2>&1); then
-			(($VERBOSE)) && printf "rsync to $COPYDIR/$i$j [OK]\n"
+		    if LOG=$($RSYNC $DRYRUN -$RSYNCPARAM -e "ssh" --progress --rsync-path="sudo rsync" $REMOTEUSER@$i:$j . 2>&1); then
+			(($VERBOSE)) && printf "$LOG\n"
+			(($VERBOSE)) && printf "[OK] rsync to $COPYDIR/$i:$j\n"
 		    else
-			printf "rsync [ERR] log: $LOG\n"
+			printf "[ERR] rsync log: $LOG\n"
 		    fi
 		else
-		    printf "does not exist. [WRN]\n"
+		    printf "[WRN] $j does not exist.\n"
 		fi
 	    done
 	else
 	    printf "[ERR] Can\'t connect to $REMOTEUSER@$i\n"
 	fi
-
     done
+    (($VERBOSE)) && printf "[OK] No more hosts.\n"
 }
 
 if [ ! -e "$COPYDIR" ]; then
     if mkdir -p $COPYDIR; then
-	(($VERBOSE)) && printf "$COPYDIR created.\n"
+	(($VERBOSE)) && printf "[OK] $COPYDIR created.\n"
     else
 	printf "[ERR] Can\'t create $COPYDIR.\n"
 	exit 1
     fi
 fi
 
-[[ "$DRYRUN" == "-n" ]] && (($VERBOSE)) && printf "* DRYRUN *\n"
+[[ "$DRYRUN" == "-n" ]] && (($VERBOSE)) && printf "* DRYRUN * DRYRUN * DRYRUN * DRYRUN * DRYRUN * DRYRUN * DRYRUN * DRYRUN * DRYRUN * DRYRUN *\n"
 
-HOSTS="srv.example.com
-mail.example.com
-www.example.com"
-DIRS="/usr/local/etc/letsencrypt/
-/var/db/ports/"
-sync
-
-HOSTS="ns1.example.com
-ns2.example.com"
-DIRS="/usr/local/etc/namedb/keys/"
-sync
+# ----------------------------------------------------------------------------
+# HOSTS="srv.example.com
+# mail.example.com
+# www.example.com"
+# DIRS="/usr/local/etc/letsencrypt/
+# /var/db/ports/"
+# sync
+#
+# HOSTS="ns1.example.com
+# ns2.example.com"
+# DIRS="/usr/local/etc/namedb/keys/"
+# sync
 
 # EOF
